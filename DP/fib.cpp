12d4be400b1a5d625cc7cb2f9011a837a3c70838
MariamMahmoud2003 <146382004+MariamMahmoud2003@users.noreply.github.com>
2025-03-12 07:28:36 -0700

    Create fib.cpp

---
+++ DP/fib.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+// dp fibonacci
+long long fib2(int n){//O(n)
+    long long arr[n+1];
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<=n;++i){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr[n];
+}
